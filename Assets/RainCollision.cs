396ddbefc4ff81b5c9dd26b1053a869b00d54a09
isabellabrorsson97 <bellabrorsson@hotmail.com>
2026-03-31 10:00:07 +0200

    RainSystem
    Created a particle system - Rain. Created folder with rain and splashsystem + script are placed.

---
+++ Assets/RainCollision.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class RainCollision : MonoBehaviour
+{
+    public GameObject SplashSystem;
+    private ParticleSystem ps;
+    private List<ParticleCollisionEvent> rainCollision;
+
+
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+        rainCollision = new List<ParticleCollisionEvent>();
+    }
+
+    // As soon as the rain hits somehting - function called
+    void OnParticleCollision(GameObject other)
+    {
+        int numberofDrops = ps.GetCollisionEvents(other, rainCollision);
+
+        //Looping through each Raindrop hit
+        for (int i = 0; i < numberofDrops; i++)
+        {
+            //Save for later - if it lags make it randomly
+            //if (Random.value > 0.3f) continue;
+
+            //exact point where the raindrop hit
+            Vector3 hitPos = rainCollision[i].intersection;
+            Vector3 hitNormal = rainCollision[i].normal;
+
+            //spawn splash - Quaternion = Rotates splash 
+            GameObject splash = Instantiate(SplashSystem, hitPos, Quaternion.LookRotation(hitNormal));
+
+            //Delete raindrop
+            Destroy(splash, 2f);
+        }
+    }
+}
