e660a8ba60054d78885f061f34771bb2f161a27c
hugo <hugo.clarin.05@gmail.com>
2026-03-31 10:28:31 +0200

    FirstInventory
    The first iteration of the inventory/clue system, currently WIP

--- Assets/Scripts/Inventory/DisplayClues.cs
+++ Assets/Scripts/Inventory/DisplayClues.cs
@@ -7,6 +7,9 @@ public class DisplayClues : MonoBehaviour
 {
     public InventoryObject inventory;
 
+    public int X_START;
+    public int Y_START;
+
     public int X_SPACE_BETWEEN_ITEM;
     public int NUMBER_OF_COLUMN;
     public int Y_SPACE_BETWEEN_ITEM;
@@ -19,7 +22,25 @@ public class DisplayClues : MonoBehaviour
 
     void Update()
     {
-        // UpdateDisplay();
+        UpdateDisplay();
+    }
+
+    public void UpdateDisplay()
+    {
+        for (int i = 0; i < inventory.Container.Count; i++)
+        {
+            if  (itemsDisplayed.ContainsKey(inventory.Container[i]))
+            {
+                // itemsDisplayed[inventory.Container[i]].GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
+            }
+            else
+            {
+                var obj = Instantiate(inventory.Container[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
+                obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
+                // obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].item.name;
+                itemsDisplayed.Add(inventory.Container[i], obj);
+            }
+        }
     }
     public void createDisplay()
     {
@@ -28,10 +49,12 @@ public class DisplayClues : MonoBehaviour
             var obj = Instantiate(inventory.Container[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
             obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
             // obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].item.name;
+
+            itemsDisplayed.Add(inventory.Container[i], obj);
         }
     }
     public Vector3 GetPosition(int i)
     {
-        return new Vector3(X_SPACE_BETWEEN_ITEM * (i % NUMBER_OF_COLUMN), Y_SPACE_BETWEEN_ITEM * (i / NUMBER_OF_COLUMN), 0);
+        return new Vector3(X_START + (X_SPACE_BETWEEN_ITEM * (i % NUMBER_OF_COLUMN)), Y_START + (-Y_SPACE_BETWEEN_ITEM * (i / NUMBER_OF_COLUMN)), 0f);
     }
 }
