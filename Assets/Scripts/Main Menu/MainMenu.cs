0a626b500e9a75e0a3153d0718c17cdb8a32edf2
isabellabrorsson97 <bellabrorsson@hotmail.com>
2026-04-13 17:54:23 +0200

    Game Menu
    Made menu for game + scripts. Start in MainMenu not MainScene anymore for correct version while playing!

---
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+    void Start()
+    {
+        //Make the mouse visible on the MainMenu scene
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    //Change scene by pressing the play button
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync("MainScene");
+    }
+
+    //Exit the game
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
