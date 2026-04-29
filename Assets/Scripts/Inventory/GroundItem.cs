3b037636c6db89362ef2b0a35bb9cd1ad24e5247
rojall98 <rojallen98@gmail.com>
2026-04-29 21:42:51 +0200

    Raycast fix in playercontroller
    Ändrade hur spelarkamerans raycast hanterar de objekt som ska ha text på skärmen t.ex 'Press E to interact with...'

--- Assets/Scripts/Inventory/GroundItem.cs
+++ Assets/Scripts/Inventory/GroundItem.cs
@@ -1,6 +1,12 @@
 using UnityEngine;
 
-public class GroundItem : MonoBehaviour
+public class GroundItem : MonoBehaviour, IInteractable
 {
     public ItemObject item;
+
+    public void Interact()
+    {
+        // Implement interaction logic here, e.g., adding the item to the player's inventory
+        Debug.Log($"Interacted with {item.name}");
+    }
 }
