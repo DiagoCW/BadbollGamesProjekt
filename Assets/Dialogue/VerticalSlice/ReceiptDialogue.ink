0dc4cc3d06b8290c6bcfac9c337df1f8e69ea667
rojall98 <rojallen98@gmail.com>
2026-05-02 22:29:12 +0200

    Interactable Objects - bigger fix

--- Assets/Dialogue/VerticalSlice/ReceiptDialogue.ink
+++ Assets/Dialogue/VerticalSlice/ReceiptDialogue.ink
@@ -1,4 +1,9 @@
 INCLUDE globalsmain.ink
-#speaker: Player
-You search the trash and find a suspicious receipt! #portrait: 0
+{foundReceipt:
+    Hey, what are you rummaging through my trash for? Stop that! #speaker: Boss Man
+- else:
+You search the trash and find a suspicious receipt. #portrait: 0 #speaker: Player
 ~ foundReceipt = true
+You found a clue! Open your inventory to examine it up close.
+Some clues unlock further dialogue options with other characters, and can even be used with other clues to produce even MORE clues!
+}
--- Assets/Scripts/Inventory/InteractableItem.cs
+++ Assets/Scripts/Inventory/InteractableItem.cs
@@ -7,24 +7,34 @@ public class InteractableItem : MonoBehaviour, IInteractable
     [SerializeField] ItemObject item;
     [SerializeField] TextAsset inkJson, inkJson2;
     HighlightActivatorIAVersion highlighter;
+    ParticleSystem particles;
 
     void Start()
     {
         playerInventory = PlayerController.Instance.inventory;
         highlighter = GameObject.FindGameObjectWithTag("Player").GetComponent<HighlightActivatorIAVersion>();
+
+        particles = GetComponentInChildren<ParticleSystem>();
+        if (particles != null)
+        {
+            particles.Play();
+            particles.Clear();
+            particles.Stop();
+        }
     }
 
     void Update()
     {
+        if (particles == null) return;
+        if (highlighter.IsHighlighting)
+            particles.Play();
+        else
+            particles.Stop();
     }
     public void Interact()
     {
-        if (inkJson != null)
-            NewDialogueManager.Instance.EnterDialogue(inkJson, null);
-
-        else if (highlighter.IsHighlighting)
+        if (highlighter.IsHighlighting)
         {
-            
             Debug.Log("Detective vision enabled, and item is interacted with");
 
             if (item != null)
@@ -32,10 +42,12 @@ public class InteractableItem : MonoBehaviour, IInteractable
                 if (inkJson2 != null)
                     NewDialogueManager.Instance.EnterDialogue(inkJson2, null);
                 playerInventory.AddItem(new Item(item));
-                Destroy(gameObject);
+                //Destroy(gameObject);
             }
             //gameObject.tag = "Untagged";
         }
+        else if (inkJson != null && !highlighter.IsHighlighting)
+            NewDialogueManager.Instance.EnterDialogue(inkJson, null);
         else Debug.Log("Detective vision NOT enabled, and item is interacted with");
     }
 }
