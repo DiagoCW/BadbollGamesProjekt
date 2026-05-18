adf00d1d3b8f772b10647ef9b5fde083c1f9ecf3
DiagoCW <stefankenpac@gmail.com>
2026-05-18 17:42:55 +0200

    Further work on FadeInOut and guide in pause menu
    Made that the screen is black when going to main scene until the trigger in the dialogue.
    Also started working on a "Field Manual" in the pause menu that guides the player of what they are supposed to do

---
+++ Assets/Scripts/Main Menu/FieldManual.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+/// <summary>
+/// Controls the tutorial menu. Allows the palyer to click through multiple pages of instructions.
+/// </summary>
+public class FieldManual : MonoBehaviour
+{
+    [Header("Instruction pages")]
+    [Tooltip("Drag the UI game objects for each page here in order p. 1, 2, 3, etc.")]
+    [SerializeField] private GameObject[] pages;
+
+    private int currentPageIndex = 0;
+
+    private void OnEnable()
+    {
+        ShowPage(0);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) NextPage(); // Left click makes the page go forward
+        else if (Input.GetMouseButtonDown(1)) PreviousPage(); // Right click makes page go backward
+    }
+
+    /// <summary>
+    /// Jumps to the next page
+    /// </summary>
+    public void NextPage() 
+    {
+        if(currentPageIndex < pages.Length - 1) 
+        {
+            ShowPage(currentPageIndex + 1);
+        }
+        else 
+        {
+            gameObject.SetActive(false);   
+        }
+    }
+
+    public void PreviousPage() 
+    {
+        if (currentPageIndex > 0) 
+        {
+            ShowPage(currentPageIndex - 1);
+        }
+    }
+
+    private void ShowPage(int index) 
+    {
+        currentPageIndex = index;
+
+        for (int i = 0; i < pages.Length; i++) 
+        {
+            pages[i].SetActive(i == currentPageIndex);
+        }
+    }
+}
